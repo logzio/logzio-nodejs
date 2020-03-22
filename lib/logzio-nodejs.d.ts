61100c70e915a04cf3b07837269df29c56040bd3
Roni Shaham <ronish31@gmail.com>
2020-03-22 19:20:06 +0200

    Add typescript support (#70)
    * add typescript support
    
    * add d.ts module support
    
    * Delete logzio-nodejs.d.ts
    
    * pr changes
    
    * pr changes
    
    * update packages
    
    * update packages
    
    * update nock version
    
    * Update Travis tests
    
    * package update
    
    * Updated version
    
    * update packages

---
+++ lib/logzio-nodejs.d.ts
@@ -0,0 +1,29 @@
+interface ILoggerOptions {
+    token: string;
+    host?: string;
+    type? : string;
+    sendIntervalMs? : number;
+    bufferSize? : number;
+    debug? : boolean;
+    numberOfRetries? : number;
+    supressErrors?  : boolean;
+    addTimestampWithNanoSecs? : boolean;
+    compress? : boolean;
+    internalLogger? : { log(message: string, ...args): any } & Record<string, any>;
+    protocol? : string;
+    port : string;
+    timeout? : number;
+    sleepUntilNextRetry? : number;
+    callback(err: Error, bulk: object): void;
+    extraFields? : {};
+}
+
+interface ILogzioLogger extends ILoggerOptions{
+    jsonToString(json: string): string;
+    log(msg: string): void;
+    close(): void;
+    sendAndClose(callback: (error: Error, bulk: object) => void): void;
+}
+
+export function createLogger(options: ILoggerOptions): ILogzioLogger;
+export function jsonToString(json: string): string;
