d8fa944ba88e9d62ab86a92367367ea755cef6bd
Shean de Montigny-Desautels <shean.desautels@hotmail.com>
2020-04-08 03:35:29 -0400

    Update Typescript declaration for optional options (#81)
    * Update type declaration for optional options
    
    * Updated version
    
    Co-authored-by: Shean de Montigny-Desautels <s.montigny-desautels@softdb.com>

--- lib/logzio-nodejs.d.ts
+++ lib/logzio-nodejs.d.ts
@@ -1,24 +1,24 @@
 interface ILoggerOptions {
     token: string;
     host?: string;
-    type? : string;
-    sendIntervalMs? : number;
-    bufferSize? : number;
-    debug? : boolean;
-    numberOfRetries? : number;
-    supressErrors?  : boolean;
-    addTimestampWithNanoSecs? : boolean;
-    compress? : boolean;
-    internalLogger? : { log(message: string, ...args): any } & Record<string, any>;
-    protocol? : string;
-    port : string;
-    timeout? : number;
-    sleepUntilNextRetry? : number;
-    callback(err: Error, bulk: object): void;
-    extraFields? : {};
+    type?: string;
+    sendIntervalMs?: number;
+    bufferSize?: number;
+    debug?: boolean;
+    numberOfRetries?: number;
+    supressErrors?: boolean;
+    addTimestampWithNanoSecs?: boolean;
+    compress?: boolean;
+    internalLogger?: { log(message: string, ...args: any[]): any } & Record<string, any>;
+    protocol?: string;
+    port?: string;
+    timeout?: number;
+    sleepUntilNextRetry?: number;
+    callback?: (err: Error, bulk: object) => void;
+    extraFields?: {};
 }
 
-interface ILogzioLogger extends ILoggerOptions{
+interface ILogzioLogger extends ILoggerOptions {
     jsonToString(json: string): string;
     log(msg: string, obj?: object): void;
     close(): void;
